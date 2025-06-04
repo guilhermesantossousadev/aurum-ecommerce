441a78ab285a91d29fa64208855ff65f5464e7d8
Guilherme santos <guilhermesantossousa08@gmail.com>
2025-06-04 16:55:25 -0300

    commit

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,6 +1,18 @@
 import "../styles/Contato.css";
 import contatoimg from "../images/contatoimg.jpeg";
 
+import anel from "../images/AnelHome.jpg";
+import { Link } from "react-router-dom";
+
+import Assinatura from "../images/AssinaturaEstética.jpg";
+import Presenca from "../images/PresençaSensorial.jpg";
+import Simbolos from "../images/SímbolosVivos.jpg";
+import Tiara from "../images/TiaraVladimir.jpg";
+import Estilo from "../images/EstilocomSignificado.jpg";
+import Hope from "../images/Hope.jpg";
+import Vinculo from "../images/VínculoSingular.png";
+import Essencia from "../images/Essencia.jpeg";
+
 function Contato() {
   return (
     <>
@@ -60,6 +72,142 @@ function Contato() {
                     </ul>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="Contato__offices">
+          <div className="Contato__offices__Horizontal__part">
+            <div className="Contato__offices__Horizontal__part__inside"></div>
+          </div>
+          {/*Box Container 1*/}
+          <div className="Contato__Box__container">
+            <div className="Contato__Box__item first">
+              <h1>OFFICES</h1>
+            </div>
+            <div className="Contato__Box__item">
+              <div className="Contato__Box__item__top">
+                <div className="Contato__Box__item__top__img">
+                  <img src={Hope} alt="Hope" />
+                </div>
+              </div>
+              <div className="Contato__Box__item__bottom">
+                <h1>San Diego</h1>
+                <p>350 Tenth Ave Suite 700 San Diego, CA 92101</p>
+              </div>
+            </div>
+            <div className="Contato__Box__item">
+              {" "}
+              <div className="Contato__Box__item__top">
+                <div className="Contato__Box__item__top__img">
+                  <img src={Tiara} alt="Tiara" />
+                </div>
+              </div>
+              <div className="Contato__Box__item__bottom">
+                <h1>New York</h1>
+                <p>36 E 20th St #6 New York, NY 10003</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="Contato__Horizontal__part">
+            <div className="Contato__Horizontal__part__inside"></div>
+          </div>
+
+          {/*Box Container 2*/}
+          <div className="Contato__Box__container">
+            <div className="Contato__Box__item first"></div>
+            <div className="Contato__Box__item">
+              <div className="Contato__Box__item__top">
+                <div className="Contato__Box__item__top__img">
+                  <img src={Assinatura} alt="Assinatura" />
+                </div>
+              </div>
+              <div className="Contato__Box__item__bottom">
+                <h1>Bay Area</h1>
+                <p>*The location of this office is undisclosed.</p>
+              </div>
+            </div>
+            <div className="Contato__Box__item">
+              {" "}
+              <div className="Box__item__top">
+                <div className="Box__item__top__img">
+                  <img src={Presenca} alt="Presença" />
+                </div>
+              </div>
+              <div className="Contato__Box__item__bottom">
+                <h1>St. Louis</h1>
+                <p>4814 Washington Blvd Studio 240 St. Louis, Missouri 63108</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="Contato__Horizontal__part">
+            <div className="Contato__Horizontal__part__inside"></div>
+          </div>
+
+          {/*Box Container 3*/}
+          <div className="Contato__Box__container">
+            <div className="Contato__Box__item first"></div>
+            <div className="Contato__Box__item">
+              <div className="Contato__Box__item__top">
+                <div className="Contato__Box__item__top__img">
+                  <img src={Simbolos} alt="Simbolos" />
+                </div>
+              </div>
+              <div className="Contato__Box__item__bottom">
+                <h1>Amsterdam</h1>
+                <p>Generaal Vetterstraat 66 1059 BW Amsterdam</p>
+              </div>
+            </div>
+            <div className="Contato__Box__item">
+              <div className="Contato__Box__item__top">
+                <div className="Contato__Box__item__top__img">
+                  <img src={Estilo} alt="Estilo" />
+                </div>
+              </div>
+              <div className="Contato__Box__item__bottom">
+                <h1>London</h1>
+                <p>
+                  Floor 9 & 10, Featherstone Building 66 City Rd, London EC1Y
+                  2AL, United Kingdom
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="Contato__Horizontal__part">
+            <div className="Contato__Horizontal__part__inside"></div>
+          </div>
+
+          {/*Box Container 4*/}
+          <div className="Contato__Box__container">
+            <div className="Contato__Box__item first"></div>
+            <div className="Contato__Box__item">
+              <div className="Contato__Box__item__top">
+                <div className="Contato__Box__item__top__img">
+                  <img src={Vinculo} alt="Vinculo" />
+                </div>
+              </div>
+              <div className="Contato__Box__item__bottom">
+                <h1>Berlin</h1>
+                <p>Hagelberger Str. 53-54 10965 Berlin</p>
+              </div>
+            </div>
+            <div className="Contato__Box__item">
+              {" "}
+              <div className="Contato__Box__item__top">
+                <div className="Contato__Box__item__top__img">
+                  <img src={Essencia} alt="Essencia" />
+                </div>
+              </div>
+              <div className="Contato__Box__item__bottom">
+                <h1>Argentina</h1>
+                <p>
+                  Garay 1802, B7600 Mar del Plata Provincia de Buenos Aires,
+                  Argentina
+                </p>
               </div>
             </div>
           </div>
